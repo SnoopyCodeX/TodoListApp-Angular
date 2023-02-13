1ec1dfbe8f9e8b3ddcff6a3d8b0120dfce1b47d0
SnoopyCodeX <extremeclasherph@gmail.com>
2023-02-13 14:20:15 +0800

    Refactored task model

--- src/app/model/task.ts
+++ src/app/model/task.ts
@@ -1,5 +1,23 @@
 export class Task {
-  id: number = 0;
-  task_name: string = "";
   done: boolean = false;
+  isDeleted: boolean = false;
+  isOpen: boolean = false;
+  id: number | null;
+
+  constructor(id: number, public task_name: string) {
+    this.id = id;
+  }
+
+  deleted() {
+    this.isDeleted = true;
+    this.id = null;
+  }
+
+  open() {
+    this.isOpen = true;
+  }
+
+  close() {
+    this.isOpen = false;
+  }
 }
